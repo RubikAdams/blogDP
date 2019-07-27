797a0b941eca1e6fba4e562c4b15da0b9c58165d
rubik <rubikadams@gmail.com>
2019-07-27 12:33:15 -0500

    hicimos quitar bandera de debug a falso

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'ra8r#v*te--k=5z*jc&$ny#m*j!ca7lfhkw&#ksos#67dyso#h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
